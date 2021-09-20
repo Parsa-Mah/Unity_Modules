a814029ae6bf9d5bace8ff0b30537f8f4114d5ba
Parsa Mahmoodi <m.n.aparsam@gmail.com>
2021-09-21 01:44:59 +0430

    Time and Date models (Switched to OOP from struct based programing to implement a MVP Architecture and more flexibility)

---
+++ Unity/Time_Date_System/Models/DateModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace PM.Utill.Date
+{
+    class DateModel
+    {
+        public int day, month, year;
+
+        private readonly string[] stringMonths = {"January", "February", "March", "April",
+            "May", "June", "July", "August", "September", "October", "November", "December"};
+
+        private readonly List<int> monthsWith30Days = new List<int> {4, 6, 9, 11};
+
+        public DateModel(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+
+        public bool IsMonth30Days(int month)
+        {
+            return monthsWith30Days.Contains(month);
+        }
+
+        public string DateToString(int type)
+        {
+            return type switch
+            {
+                0 => string.Format("{0}/{1}/{2}", day, month, year),
+                1 => string.Format("{0}/{1}/{2}", day, stringMonths[month--], year),
+                _ => "Error date TYPE not recognized or not passed correctly",
+            };
+        } 
+    }
+}
---
+++ Unity/Time_Date_System/Models/TimeModel.cs
@@ -0,0 +1,18 @@
+﻿
+namespace PM.Utill.Time
+{
+    class TimeModel
+    {
+        public int hou, min, sec;
+
+        public TimeModel(int hou, int min, int sec)
+        {
+            this.hou = hou;
+            this.min = min;
+            this.sec = sec;
+        }
+
+        public override string ToString() =>
+            string.Format($"{hou.ToString("00")}:{min.ToString("00")}.{sec.ToString("00")}");
+    }
+}
