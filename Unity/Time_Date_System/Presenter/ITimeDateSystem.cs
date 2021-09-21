68880cb0e7cee8f984ca8b40521a478a43fd8621
Parsa Mahmoodi <m.n.aparsam@gmail.com>
2021-09-22 00:00:08 +0330

    Switched to a better more simple namespace approach

---
+++ Unity/Time_Date_System/Presenter/ITimeDateSystem.cs
@@ -0,0 +1,22 @@
+﻿namespace PM.Util.Time_Date_System
+{
+    public interface ITimeDateSystem
+    {
+        // Init both objects
+        void SetDate(int day, int month, int year);
+        void SetTime(int hou, int min, int sec);
+        // Part of Time object
+        void AddOneSecond(int time);
+        void AddOneMinute();
+        void AddOneHour();
+        // Part of Date Object
+        string AddOneDay();
+        string AddOneMonth();
+        string AddOneYear();
+        // returns time and Date in string
+        string GetTime();
+        public string GetDate(int type);
+        // Starts the time
+        void StartCountingTime(int time);
+    }
+}
