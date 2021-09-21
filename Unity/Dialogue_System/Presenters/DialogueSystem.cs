2bcbece609d8981d4324d239e4dcb0f3f1dd881c
Parsa Mahmoodi <m.n.aparsam@gmail.com>
2021-09-22 00:00:23 +0330

    Incompleted implementation

---
+++ Unity/Dialogue_System/Presenters/DialogueSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PM.Dialogue_System.Models;
+
+namespace PM.Util.Dialogue_System
+{
+    class DialogueSystem
+    {
+        public ConversationModel conversationModel;
+
+        public DialogueSystem()
+        {
+            this.conversationModel = new ConversationModel();
+        }
+
+        public void AddNPC(string name, int age = 18)
+        {
+            int _id = conversationModel.npcList.Count();
+            NPCModel npcModel = new NPCModel(_id, name, age);
+            conversationModel.npcList.Add(npcModel);
+        }
+
+        public void AddDialogue(string dialogueText, int timeToShow)
+        {
+            DialogueModel dialogueModel = new DialogueModel(dialogueText, timeToShow);
+            conversationModel.dialogueList.Add(dialogueModel);
+        }
+    }
+}
